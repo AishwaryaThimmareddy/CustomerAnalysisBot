d91c385b06b43ab72b9923ed12808d57bd19e0e3
AishwaryaThimmareddy <168058012+AishwaryaThimmareddy@users.noreply.github.com>
2024-05-20 15:25:20 +0530

    updated

--- Default.py
+++ Default.py
@@ -110,7 +110,7 @@ def set_png_as_page_bg(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
  
-set_png_as_page_bg(r'background.png')
+set_png_as_page_bg(r'background2.jpg')
  
 @st.cache_resource()
 def initialize_rag():
